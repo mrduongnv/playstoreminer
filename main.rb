72decd510c493d8049b2bd3bd943a747ff9e168b
Duong Nguyen <mrduongnv@gmail.com>
2016-03-11 15:20:15 +0700

    Update MongoDB data organization

--- main.rb
+++ main.rb
@@ -17,10 +17,13 @@ DATABASE = 'playstore_' + DateTime.parse(Time.now.to_s).strftime("%Y_%m_%d")
 def get_unfetch_app
   database = @client.database
   database.collection_names.each do |name|
-    @apps_id << name[10..-1] if @client[name].find({:_state => 0}).count == 1
+    #@apps_id << name[10..-1] if @client[name].find({:_state => 0}).count == 1
   end
   @apps_id << "com.sd.google.helloKittyCafe"
+  @client["app_list"].insert_one({:_id => "com.sd.google.helloKittyCafe", :package => "com.sd.google.helloKittyCafe"})#in waiting state
 end
+
+
 
 
 def scrape(id)
@@ -35,7 +38,7 @@ def scrape(id)
 
     page = Nokogiri::HTML(@data)
 
-    record = {}
+    record = {:_id => id, :package => id}
     app_title_css_path = 'html body.no-focus-outline div#wrapper.wrapper.wrapper-with-footer div#body-content.body-content div.outer-container div.inner-container div.main-content div div.details-wrapper.apps.square-cover.id-track-partial-impression.id-deep-link-item div.details-info div.info-container div.info-box-top h1.document-title div.id-app-title'
     app_images_css_path = 'html body.no-focus-outline div#wrapper.wrapper.wrapper-with-footer div#body-content.body-content div.outer-container div.inner-container div.main-content div div.details-wrapper.apps.square-cover.id-track-partial-impression.id-deep-link-item div.details-section.screenshots div.details-section-contents div.details-section-body.expandable div.thumbnails-wrapper div.thumbnails img.screenshot'
     app_desc_css_path = 'html body.no-focus-outline div#wrapper.wrapper.wrapper-with-footer div#body-content.body-content div.outer-container div.inner-container div.main-content div div.details-wrapper.apps.square-cover.id-track-partial-impression.id-deep-link-item div.details-section.description.simple.contains-text-link.apps-secondary-color div.details-section-contents.show-more-container.apps-description div div.show-more-content.text-body'
@@ -89,11 +92,11 @@ def scrape(id)
     page.css(app_suggest_app_css_path).each do |el|
       #record["desc"] = el.text el['title']
       next_app = el['href'][23..-1]
-      #if @client["app_state_" + next_app].find({:_state => 0}).count == 0 && @client["app_state_" + next_app].find({:_state => 1}).count == 0 # Not in queue
-      #  @client["app_state_" + next_app].insert_one({_state: 0})#in waiting state
-      #  @apps_id << c
-      #send
-      #puts next_app
+      if @client["app_list"].find({:_id => next_app}).count == 0 and not @apps_id.include?(next_app)# Not in queue
+        @client["app_list"].insert_one({:_id => next_app, :package => next_app})#in waiting state
+        @apps_id << next_app
+      end
+      puts next_app
     end
 
     update_info = []
@@ -104,7 +107,7 @@ def scrape(id)
     puts update_info
     record["info"] = update_info
     puts record
-    #@client["app_info_" + id].insert_one(record) if id.length < 100
+    @client["app_info"].insert_one(record)
   rescue
     puts id
   end
